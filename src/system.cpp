ca1241d35ad361e811c0b5f0401abe6b80b5479f
Wolf-Peter Bolduan <wolf-peter@bolduan.net>
2021-12-23 11:31:32 +0100

    First running version

--- src/system.cpp
+++ src/system.cpp
@@ -17,24 +17,39 @@ using std::vector;
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+// Return a container/vector composed of the system's processes
+// With help from Mentor page
+// https://knowledge.udacity.com/questions/270856
+//
 
-// TODO: Return the system's kernel identifier (string)
+vector<Process>& System::Processes() { 
+  vector<int> pids = LinuxParser::Pids();
+  processes_.clear();
+  for (long unsigned int i = 0; i < pids.size(); i++) {
+    if (!LinuxParser::Ram(pids[i]).empty()) {
+      Process process(pids[i]);
+      processes_.push_back(process);
+    }
+  }  
+  std::sort(processes_.rbegin(), processes_.rend());
+  return processes_;
+}
+ 
+// DONE: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
-// TODO: Return the system's memory utilization
+// DONE: Return the system's memory utilization
 float System::MemoryUtilization() { 
     return LinuxParser::MemoryUtilization();
 }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
+// DONE: Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem();}
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+// DONE: Return the number of processes actively running on the system
+int System::RunningProcesses() { return LinuxParser::RunningProcesses();}
 
-// TODO: Return the total number of processes on the system
+// DONE: Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses();}
 
 // TODO: Return the number of seconds since the system started running
